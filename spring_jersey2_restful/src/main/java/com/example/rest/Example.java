b26a1ae7d0d62fc48212b0911c272c8a71573bce
linked2ev <linked2ev@gmail.com>
2019-04-15 00:45:36 +0900

    RESTful Jersey + Spring4

--- spring_jersey2_restful/src/main/java/com/example/rest/Example.java
+++ spring_jersey2_restful/src/main/java/com/example/rest/Example.java
@@ -1,14 +1,42 @@
 package com.example.rest;
 
+import java.util.HashMap;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/example")
-public class Example
-{
-    @GET
-    public String getMsg()
-    {
-         return "Hello Example!! - Jersey 2";
-    }
+@Path("example")
+public class Example {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getMsg() {
+		String rmsg = "Hello RESTful ! - Jersey 2 + Spring4";
+		return Response.status(200).entity(rmsg).build();
+	}
+	
+	@GET @Path("{str}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getExample(@PathParam("str") String str) {
+		String rmsg = "[GET] PathParam:" + str;
+		return Response.status(200).entity(rmsg).build();
+	}
+	
+	@GET @Path("json")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getJSONExample() {
+		
+		HashMap<String, Object> rmap = new HashMap<String, Object>();
+		rmap.put("buyer_email", "test@email.com");
+		rmap.put("buyer_name", "YOUNG JUN, KIM");
+		rmap.put("buyer_tel", "01048111234");
+		rmap.put("buyer_country", "KOREA");
+		
+		return Response.status(200).entity(rmap).build();
+	}
+	
 }
--- spring_jersey2_restful/src/main/java/com/notice/rest/Notice.java
+++ spring_jersey2_restful/src/main/java/com/notice/rest/Notice.java
@@ -1,14 +1,54 @@
 package com.notice.rest;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/notice")
-public class Notice
-{
-    @GET
-    public String getMsg()
-    {
-         return "Notice REST Service !! - Jersey 2";
-    }
+import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
+
+@Component
+@Path("notice")
+public class Notice {
+
+	@GET @Path("/list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getNoticeList() {
+		String rmsg = "[GET] Example List";
+		return Response.status(200).entity(rmsg).build();
+	}
+	
+	@GET @Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getNotice(@PathParam("id") Integer id) {
+		String rmsg = "[GET] Example ID:" + id;
+		return Response.status(200).entity(rmsg).build();
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response postNotice(ModelMap p) {
+		String rmsg = "[POST] Create Notice Params: " + p;
+		return Response.status(200).entity(rmsg).build();
+	}
+	
+	@PUT @Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response putNotice(@PathParam("id") Integer id) {
+		String rmsg = "[PUT] Update Notice ID: " + id;
+		return Response.status(200).entity(rmsg).build();
+	}
+	
+	@DELETE @Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteNotice(@PathParam("id") Integer id) {
+		String rmsg = "[DELETE] Delete Notice ID: " + id;
+		return Response.status(200).entity(rmsg).build();
+	}
 }
